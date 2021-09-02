8b57a678b6d0b9155ccc45d52c6a80724b3611a5
Arafat Hossaine <arafathossain7960@gmail.comm>
2021-09-02 20:45:12 +0600

    add https

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const loadBooks=()=>{
     const searchInput = document.getElementById('search-text');
     const searchText = searchInput.value ;
     if(searchText !== ''){
-        const url = ` http://openlibrary.org/search.json?q=${searchText}`;
+        const url = ` https://openlibrary.org/search.json?q=${searchText}`;
         fetch(url)
         .then(res => res.json())
         .then(data => displayBooks(data))
