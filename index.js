81504b9f419bfa0bf3748ad2d146d284ce0ff2af
Arafat Hossaine <arafathossain7960@gmail.comm>
2021-09-02 15:43:08 +0600

    last commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // load data from API 
 const loadBooks=()=>{
+  
+  displayNoneBlock('not-found', false);
     const searchInput = document.getElementById('search-text');
     const searchText = searchInput.value ;
     if(searchText !== ''){
@@ -9,7 +11,10 @@ const loadBooks=()=>{
         .then(data => displayBooks(data))
         .catch(err => console.log(err))
         searchInput.value = '';
-        toggleSpinner('block')
+
+        displayNoneBlock('spinner', true)
+        displayNoneBlock('container', false)
+      
     }else{
         alert('Please! Enter Book Name')
     }
@@ -17,15 +22,19 @@ const loadBooks=()=>{
 // display data on UI 
 const displayBooks=(books)=>{
 const allBooks = books.docs;
-
 const bookContainer =document.getElementById('book-container');
 bookContainer.textContent = '';
-allBooks.forEach(book =>{
+if(allBooks.length == ''){
+  displayNoneBlock('not-found', true);
+  displayNoneBlock('result-count', false)
+}
+  // use forEach--
+  allBooks.forEach(book =>{
     const div = document.createElement('div');
     div.classList.add('col-md-6');
     div.innerHTML=`
     <div class="card h-100 display-style">
-    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+    <img src="https://covers.openlibrary.org/b/id/${book.cover_i ?book.cover_i: ''}-M.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h2 class="card-title">Book Name : ${book.title}</h2>
         <p>Author Name : ${book.author_name}</p>
@@ -41,9 +50,18 @@ allBooks.forEach(book =>{
     <h5 class="text-center mt-5"> Total result found :<span class="text-info"> ${books.numFound} </span></h5>
     `;
 })
-toggleSpinner('none')
+displayNoneBlock('spinner', false);
+displayNoneBlock('container', true);
+
+}
+ 
+// common function for Spinner, not-Found Result, hide previous result, hied result count -------------------
+const displayNoneBlock=(idName, Style)=>{
+  if(Style === true){
+    document.getElementById(idName).style.display='block';
+  }else if(Style === false){
+    document.getElementById(idName).style.display='none';
+  }
 }
 
-const toggleSpinner = displayStyle =>{
-    document.getElementById('spinner').style.display =displayStyle;
-}
+// Note: if you want to see not-result container please enter 000000000000000000000000000000000000000 or any digit
